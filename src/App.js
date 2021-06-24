dd5dc04b70e0c230d694327f9da8616240a4dc29
DavidRMorphew <morphewd@umich.edu>
2021-06-24 11:56:16 -0400

    commit tree 399dabdf89a56eb60ebf7de862fa27fab416ab02

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
-
+import {connect} from 'react-redux'
+import {fetchCats} from './actions/catActions'
+import CatList from './CatList'
 class App extends Component {   
-  
+
+  componentDidMount(){
+    this.props.fetchCats()
+  }
+
+  loadingOrCats = () => (
+    (this.props.loading) ? <div>Loading...</div> : <CatList catPics={this.props.catPics} />
+  )
+
   render() {
     return (
-      <div>
+      <div className="App">
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        {this.loadingOrCats()}
       </div>
     );
   }
 }
 
-export default App
+
+
+// Alternative:
+// const mapStateToProps = state => {
+//   return {
+//     catPics: state.cats,
+//     loading: state.loading
+//   }
+// }
+
+export default connect(({cats, loading})=>({catPics: cats, loading}), {fetchCats})(App)
 
